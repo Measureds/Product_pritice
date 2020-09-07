6df03c23db86f344da852e539eb9aeb584f339bd
Measured <41724056@xs.ustb.edu.cn>
2020-09-07 10:00:25 +0800

    add axios to json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import EchartsTest from './EchartsTest'
 import ReactWordcloud from "react-wordcloud";
+import List from './List';
  
 const words = [
   { text: "hello", value: 3 },
   { text: "world", value: 12.5 },
   { text: "github", value: 1 },
   { text: "code", value: 1 },
-  { text:"憨憨",value:100},
-  { text:"我猪最可爱！",value:150}
+  { text:"憨憨",value:100}
 ];
 
 function App() {
@@ -21,6 +20,9 @@ function App() {
         <ReactWordcloud words={words} />
         </div>
         <EchartsTest/>
+        <div className="container">
+        <List />
+      </div>
     </div>
   );
 }
